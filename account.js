876b60141f0c1cf0e0c6f0088e33d3527020efc7
tiluuta <tiluutan@gmail.com>
2023-04-06 19:41:31 -0700

    "contact" link - mobile:insta, desktop:linkedin

---
+++ account.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    //placeholder values - replace with real data from database
+    var rank = 1;
+    var username = 'John Doe';
+    var logins = 10;
+    var numoffriends = 11;
+    var numofposts = 12;
+
+    //implement if .json file
+    /*
+    fetch("./data.json")
+        .then(function(response) {
+        return response;
+        })
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function (Normal) {
+            const username = Normal.map(
+                (data) => '${data.username}'
+            );
+            const numoffriends = Normal.map(
+                (data) => '${data.numoffriends}'
+            );
+            const numofposts = Normal.map(
+                (data) => '${data.numofposts}'
+            );
+        })
+        .catch(function(err) {
+            console.log("Fetch problem show: " + err.message);
+        }
+        );
+    */
+
+        $("#rank").html("Your Ranking: " + rank);
+        $("#username").html(username);
+        $("#numofposts").html("Total Posts: " + numofposts);
+        $("#numoffriends").html("Total Associates: " + numoffriends);
+});
+  
