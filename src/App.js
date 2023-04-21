8e05ac7a363dc57b9ab91ed649fb70c1d04bd985
Shane Yokota <shaneyok@usc.edu>
2023-04-20 19:24:10 -0700

    pulling user from db

--- src/App.js
+++ src/App.js
@@ -17,7 +17,11 @@ import Signup from './Components/Signup';
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Outlet, Navigate, Link} from "react-router-dom";
 
-function Home({postsinfo}){
+function Home({postsinfo, usersinfo}){
+  // console.log("Home->post");
+  // console.log(postsinfo);
+  // console.log("Home->user");
+  // console.log(usersinfo);
   return(
     <div className='Mainbody'>
       <div className='threecol' id='Leaderboarddiv' tabIndex={0}>
@@ -40,8 +44,8 @@ function Home({postsinfo}){
         <h1 className='sectiontitle'>PROFILE</h1>
         <div className='threecolcontent'>
           {/* <h1>profile</h1> */}
-          {postsinfo.map(item => <Profile{...item}></Profile>)}
-          {postsinfo.map(item => <Profile{...item}></Profile>)}
+          {usersinfo.map(item => <Profile{...item}></Profile>)}
+          {usersinfo.map(item => <Profile{...item}></Profile>)}
         </div>
       </div>
     </div>
@@ -60,6 +64,7 @@ function Template(){
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(false);
@@ -68,11 +73,25 @@ function App() {
     scanTable('socio-media-posts')
       .then((items) => {
         setPosts(items);
+        console.log("posts");
+        console.log(items);
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
+  useEffect(() => {
+    scanTable('socio-media-users')
+      .then((items) => {
+        setUsers(items);
+        console.log("users");
+        console.log(items);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
 
   // run it when login button is clicked
   const Authorize = (un, pw) => {
@@ -102,7 +121,7 @@ function App() {
         </div>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
         <Route path='/' element={<ProtectedRoute><Template></Template></ProtectedRoute>}>
-          <Route path="/home" element={<Home postsinfo={posts}></Home>} />
+          <Route path="/home" element={<Home postsinfo={posts} usersinfo={users}></Home>} />
           <Route path="/about" element={<About></About>} />
           <Route path="/contact" element={<Contact></Contact>} />
           <Route path="/terms" element={<Terms></Terms>} />
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,15 +2,12 @@ import React from "react";
 import '../App.css';
 
 export default function Profile(props){
-    // const {username, totalpost, friendnum} = props
-    const friendnum = '3'
-    let name= props.Title.S
-    let namelist = name.split('-')
     return(
         <div className="innerdiv">
-            <h1>{namelist[0]}</h1>
-            <h5>{props.Content.S}</h5>
-            <h5>{friendnum}</h5>
+            <h1>{props.Username.S}</h1>
+            <h5>Posts:{props.Posts.N}</h5>
+            <h5>Friends:{props.Friends.N}</h5>
+            <h5>Enemies:{props.Enemies.N}</h5>
             <div>
             <button className="innerdivbutton">message user</button>
             <button className="innerdivbutton">flag as enemy</button>
