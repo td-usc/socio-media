eca2dfcab4874aa0257f3b4b7db149947d760d42
tiluuta <tiluutan@gmail.com>
2023-04-27 20:42:52 -0700

    Update CSS and add privacy/help pages

--- src/Components/Privacy.js
+++ src/Components/Privacy.js
@@ -3,6 +3,11 @@ import '../App.css';
 
 export default function Privacy(){
     return (
-        <h1>Privacy page</h1>
+        <div class="flexparent centercontent">
+        <h1>Privacy Policy </h1>
+        <p>Socio-Media is a public platform. Your data and content posted on Socio-Media is stored through AWS and will be used to update and improve the project. Suck it up, you're on a platform for sociopaths.</p>
+        <h2>USC Corpus Callosum Media</h2>
+            <p>Your posts, account, and all user-generated content displayed on Socio-Media is subject to use by USC Corpus Callosum as media. Corpus Callosum may use your unhinged thoughts to market, showcase, and promote the club, project, and/or members of the Socio-Media team.</p>
+        </div>
     );
 }
