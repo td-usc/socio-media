35d4e18447864fd8281fdd88d8084e9ff69efde8
tiluuta <tiluutan@gmail.com>
2023-04-28 00:43:28 -0700

    using Cookies for login

--- src/Components/Myaccount.js
+++ src/Components/Myaccount.js
@@ -2,24 +2,49 @@ import React from "react";
 import '../App.css';
 import {Link} from "react-router-dom";
 
-export default function Account(){
+export default function Account({un}){
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        scanTable('socio-media-users')
+          .then((items) => {
+            for(let i=0; i<items.length; i++){
+                if(items[i].Username.S === un){
+                    setUsers([[items[i].Username.S], [items[i].Posts.N], [items[i].Friends.N], [items[i].Enemies.N]]);
+                    break;
+                }
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+    }, []);
+
+    const logOut = () => {
+        localStorage.clear();
+    }
+
+
     return(
-        <div class="board flex" id="profileboard">
-            <div class="centercontainer">
-                    <img id="profile" src="https://debubglobalbank.com/wp-content/uploads/2019/08/blank-profile-picture.png" alt="profile pic"></img>
-                    <p id="username">
-                        "Username"
-                    </p>
-                    <p id="numofposts">
-                        "Number of Total Posts"
-                    </p>
-                    <p id="numoffriends">
-                        "Number of Total Friends"
-                    </p>
-                    <p id="numofenemies">
-                        "Number of Total Enemies"
-                    </p>
-                    <Link className="flexnavbutt" to="/reset" id="resetlink">RESET ACCOUNT</Link>
+        <div className="accountparentdiv">
+            <div class="board flex" id="profileboard">
+                <div class="centercontainer">
+                        <img id="profile" src="https://debubglobalbank.com/wp-content/uploads/2019/08/blank-profile-picture.png" alt="profile pic"></img>
+                        <p id="username">
+                            <b>Username</b>: {users[0]}
+                        </p>
+                        <p id="numofposts">
+                            <b>Number of Total Posts</b>: {users[1]}
+                        </p>
+                        <p id="numoffriends">
+                            <b>Number of Total Friends</b>: {users[2]}
+                        </p>
+                        <p id="numofenemies">
+                            <b>Number of Total Enemies</b>: {users[2]}
+                        </p>
+                        <Link className="flexnavbutt" to="/reset" id="resetlink">RESET ACCOUNT</Link>
+                        <button id="logout" onClick={() => logOut()}> <Link to="/login">Log Out</Link> </button>
+                </div>
             </div>
         </div>
     );
