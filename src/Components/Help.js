5c5b09447e09c6a35b5f8d4a8df58ad6bc8c61a8
Phoebe Turner <peturner@usc.edu>
2023-04-27 21:04:44 -0700

    Capitalization of html pages

--- src/Components/Help.js
+++ src/Components/Help.js
@@ -4,7 +4,7 @@ import '../App.css';
 export default function Help(){
     return(
         <div class="flexparent centercontent">
-        <h1>Help page</h1>
+        <h1>Help Page</h1>
         <p>We can't help you.</p>
         </div>
     );
