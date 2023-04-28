3e3eb69ca6e74205f1d5b9edb4f7af09a572ec32
tiluuta <tiluutan@gmail.com>
2023-04-27 21:43:13 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/td-usc/socio-media""
    This reverts commit 2537d359f8687d9294dd3644d2e63c6f601f953e.
    
    Merging

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../App.css';
+import {updateTableItem} from '../AWSFunctions'
 
 export default function Profile(props){
     return(
@@ -10,8 +11,8 @@ export default function Profile(props){
             <h5>Enemies:{props.Enemies.N}</h5>
             <div>
             
-            <button className="innerdivbutton">flag as enemy</button>
-            <button className="innerdivbutton">add as aquaintance</button>
+            <button className="innerdivbutton" onClick={() => updateTableItem('socio-media-users', props.Username.S, 'Enemies')}>flag as enemy</button>
+            <button className="innerdivbutton" onClick={() => updateTableItem('socio-media-users', props.Username.S, 'Friends')}>add as aquaintance</button>
             <button className="innerdivbutton">report</button>
             </div>
       </div>
